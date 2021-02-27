16ad812b9d9c94a4cd4fe0af4c1da369db28e269
Andrey Kurashev <andreykurashev@gmail.com>
2021-02-27 19:26:50 +0000

    Make Todo Server registered

--- Chapter 5. Concurrency primitives/todo_server.ex
+++ Chapter 5. Concurrency primitives/todo_server.ex
@@ -1,24 +1,26 @@
 defmodule TodoServer do
   def start do
-    spawn(fn ->
+    pid = spawn(fn ->
       loop(TodoList.new)
     end)
+    Process.register(pid, :todo_server)
+    :todo_server
   end
 
-  def add_entry(pid, entry) do
-    send(pid, {:add_entry, entry})
+  def add_entry(entry) do
+    send(:todo_server, {:add_entry, entry})
   end
 
-  def update_entry(pid, entry_id, updater_fun) do
-    send(pid, {:update_entry, entry_id, updater_fun})
+  def update_entry(entry_id, updater_fun) do
+    send(:todo_server, {:update_entry, entry_id, updater_fun})
   end
 
-  def delete_entry(pid, entry_id) do
-    send(pid, {:delete_entry, entry_id})
+  def delete_entry(entry_id) do
+    send(:todo_server, {:delete_entry, entry_id})
   end
 
-  def entries(pid, date) do
-    send(pid, {:entries, self(), date})
+  def entries(date) do
+    send(:todo_server, {:entries, self(), date})
 
     receive do
       {:todo_entries, entries} -> entries
