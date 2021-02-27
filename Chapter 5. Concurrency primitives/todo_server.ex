fc13745fd98a34b4d0d3e6f3c4b5ca3f048a1a6f
Andrey Kurashev <andreykurashev@gmail.com>
2021-02-27 19:19:24 +0000

    Implement Todo List server

---
+++ Chapter 5. Concurrency primitives/todo_server.ex
@@ -0,0 +1,107 @@
+defmodule TodoServer do
+  def start do
+    spawn(fn ->
+      loop(TodoList.new)
+    end)
+  end
+
+  def add_entry(pid, entry) do
+    send(pid, {:add_entry, entry})
+  end
+
+  def update_entry(pid, entry_id, updater_fun) do
+    send(pid, {:update_entry, entry_id, updater_fun})
+  end
+
+  def delete_entry(pid, entry_id) do
+    send(pid, {:delete_entry, entry_id})
+  end
+
+  def entries(pid, date) do
+    send(pid, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(todo_list) do
+    new_todo_list = receive do
+      message -> process_message(todo_list, message)
+    end
+
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add_entry, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp process_message(todo_list, {:update_entry, entry_id, updater_fun}) do
+    TodoList.update_entry(todo_list, entry_id, updater_fun)
+  end
+
+  defp process_message(todo_list, {:delete_entry, entry_id}) do
+    TodoList.delete_entry(todo_list, entry_id)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
+  end
+end
+
+defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.auto_id,
+        entry
+      )
+
+    %TodoList{entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id, title: _, date: _} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
+  end
+end
