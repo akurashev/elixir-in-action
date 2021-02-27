d0bafae0de81e3c358161b62d5b4ffcbfacea561
Andrey Kurashev <andreykurashev@gmail.com>
2021-02-27 19:39:30 +0000

    Add process bottleneck example

---
+++ Chapter 5. Concurrency primitives/process_bottleneck.ex
@@ -0,0 +1,20 @@
+defmodule Server do
+  def start, do: spawn(&loop/0)
+
+  def send_msg(server, message) do
+    send(server, {self(), message})
+
+    receive do
+      {:response, response} -> response
+    end
+  end
+
+  defp loop do
+    receive do
+      {caller, msg} ->
+        Process.sleep(1000)
+        send(caller, {:response, msg})
+    end
+    loop()
+  end
+end
