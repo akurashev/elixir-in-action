2883398be6026d744e35fb949f48228fd1665203
Andrey Kurashev <andreykurashev@gmail.com>
2021-02-28 10:15:24 +0000

    Rewrite Todo List Server using custom generic server process

---
+++ Chapter 6. Generic server processes/server_process_todo.ex
@@ -0,0 +1,127 @@
+defmodule ServerProcess do
+  def start(callback_module) do
+    spawn(fn ->
+      initial_state = callback_module.init()
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  def call(server_pid, request) do
+    send(server_pid, {:call, request, self()})
+
+    receive do
+      {:response, response} -> response
+    end
+  end
+
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
+  end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {:call, request, caller} ->
+        {response, new_state} = callback_module.handle_call(request, current_state)
+
+        send(caller, {:response, response})
+        loop(callback_module, new_state)
+
+      {:cast, request} ->
+        new_state = callback_module.handle_cast(request, current_state)
+        loop(callback_module, new_state)
+    end
+  end
+end
+
+defmodule TodoServer do
+  def start do
+    ServerProcess.start(TodoServer)
+  end
+
+  def add_entry(pid, entry) do
+    ServerProcess.cast(pid, {:add_entry, entry})
+  end
+
+  def update_entry(pid, entry_id, updater_fun) do
+    ServerProcess.cast(pid, {:update_entry, entry_id, updater_fun})
+  end
+
+  def delete_entry(pid, entry_id) do
+    ServerProcess.cast(pid, {:delete_entry, entry_id})
+  end
+
+  def entries(pid, date) do
+    ServerProcess.call(pid, {:entries, date})
+  end
+
+  def init(), do: TodoList.new
+
+  def handle_cast({:add_entry, entry}, todo_list) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  def handle_cast({:update_entry, entry_id, updater_fun}, todo_list) do
+    TodoList.update_entry(todo_list, entry_id, updater_fun)
+  end
+
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
+    TodoList.delete_entry(todo_list, entry_id)
+  end
+
+  def handle_call({:entries, date}, todo_list) do
+    {TodoList.entries(todo_list, date), todo_list}
+  end
+end
+
+defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.auto_id,
+        entry
+      )
+
+    %TodoList{entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id, title: _, date: _} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
+  end
+end
